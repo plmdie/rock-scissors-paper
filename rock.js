1dd238b7c6147c2d6d86c0323fe80c6e21d8087b
plmdie <piratasdoarenitoexplosiv0@protonmail.com>
2022-04-07 20:27:56 +0200

    Function player selection, function play individual round

--- rock.js
+++ rock.js
@@ -12,3 +12,55 @@ function playerPlay() {
     playerChoice = playerChoice.toLowerCase();
     return playerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'rock': 
+            switch(computerSelection) {
+                case 'rock': 
+                    console.log('The game is tied, both players choosed Rock.');
+                    return 'tie';
+                
+                case 'scissors': 
+                    console.log('The player has won, Rock smashes Scissors.');
+                    return 'player';
+                
+                case 'paper': 
+                    console.log('The computer has won, Paper wraps the Rock.');
+                    return 'computer';
+            }
+        case 'scissors': 
+            switch(computerSelection) {
+                case 'rock': 
+                    console.log('The computer has won, Rock smashes Scissors.');
+                    return 'computer';
+                
+                case 'scissors': 
+                    console.log('The game is tied, both players choosed Scissors.');
+                    return 'tie';
+                
+                case 'paper': 
+                    console.log('The players has won, Scissors cut Paper.');
+                    return 'player';
+                
+            }
+        
+        case 'paper': 
+            switch(computerSelection) {
+                case 'rock': 
+                    console.log('The player has won, Paper wraps the Rock.');
+                    return 'player';
+                
+                case 'scissors': 
+                    console.log('The computer has won,  Scissors cut Paper.');
+                    return 'computer';
+                
+                case 'paper': 
+                    console.log('The game is tied, both players choosed Paper.');
+                    return 'tie';
+                
+            }
+        
+    }
+    
+}
