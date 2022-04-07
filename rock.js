9ca05a87714283aac69ebf428a97cd24e93bfd53
plmdie <piratasdoarenitoexplosiv0@protonmail.com>
2022-04-07 21:36:45 +0200

    Counting each player score and announce winner

--- rock.js
+++ rock.js
@@ -1,3 +1,5 @@
+/* Get computer choice randomly */
+
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
@@ -7,11 +9,15 @@ function computerPlay() {
     }
 }
 
+/* Prompt player for their choice */
+
 function playerPlay() {
     let playerChoice = prompt('Choose with what you want to player, Rock Scissors or Paper:');
     playerChoice = playerChoice.toLowerCase();
     return playerChoice;
 }
+
+/* Play individual round and return winner */
 
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
@@ -64,3 +70,40 @@ function playRound(playerSelection, computerSelection) {
     }
     
 }
+
+/* Counting each player score and announce winner */
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+
+        /* Show Round # and players choice */
+        console.log(`Round ${i}`);
+        console.log(`Player choosed ${playerSelection}`);
+        console.log(`Computer choosed ${computerSelection}`);
+        
+        /* Get result of round */
+        let result = playRound(playerSelection, computerSelection);
+        /* Count score */
+        if (result === 'computer') { computerScore += 1; }
+        if (result === 'player') { playerScore += 1; }
+        console.log(`Computer Score ${computerScore}`);
+        console.log(`Player Score ${playerScore}`);
+
+        /* Stating game victory */ 
+        if (i === 5) {
+            if (computerScore > playerScore) { console.log(`Computer won the game. ${computerScore} rounds.`); }
+            else if (playerScore > computerScore) {
+                console.log(`Player won ${playerScore} rounds.`);
+                console.log(`round: ${i}`);
+            }
+            else {console.log(`The game was tied, ${computerScore} vs ${playerScore}.`);}
+        }   
+    } 
+}
+
+game();
